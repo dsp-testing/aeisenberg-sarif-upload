b293d987e63a2f7880cc26e51bdd78eadf69e2e7
Andrew Eisenberg <aeisenberg@github.com>
2025-06-25 15:43:09 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ function setLanguageOptions() {
     // TODO sanitize the string by importing the dompurify library
     var href = document.location.href,
         deflt = href.substring(href.indexOf("default=")+8);
-    document.write("<OPTION value=1>"+deflt+"</OPTION>");
+    document.write("<OPTION value=1>" + deflt + "</OPTION>");
     document.write("<OPTION value=2>English</OPTION>");
 }
